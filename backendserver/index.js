f37837daf50eb454e48df0a382076d8e8aef9c9a
SohamSarkar06 <sohamsage0610@gmail.com>
2025-06-08 21:12:47 +0530

    Create index.js

---
+++ backendserver/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("User connected: ", socket.id);
+
+  socket.on("send_message", (data) => {
+    console.log("Received message from client:", data);
+    socket.broadcast.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id);
+  });
+});
+
+
+server.listen(4000, () => {
+  console.log("Server running on http://localhost:4000");
+});
